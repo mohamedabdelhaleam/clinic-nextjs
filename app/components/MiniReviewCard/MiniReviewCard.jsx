f92cca13834d9999f5ce4726e08e5426ef55681d
Halim <mohameabdelhaleam13@gmail.com>
2023-08-01 13:59:12 +0300

    upload project

--- app/components/MiniReviewCard/MiniReviewCard.jsx
+++ app/components/MiniReviewCard/MiniReviewCard.jsx
@@ -5,7 +5,7 @@ const MiniReviewCard = () => {
     <div className='bg-[#CE4642] rounded-md max-w-[640px] px-8 py-14 h-[240px] shadow-md'>
         <div className='flex justify-between items-center flex-col h-full'>
             <div className='text-white text-xl text-right'>مهما قلت لن أوفيك حقك يا دكتور مؤمن، لولا الجلسات اللي عملتهالي الفترة اللي فاتت مكنتش هبقى قادر أمشي دلوقتي .. ربنا يجازيك خيراً ويبارك لك في صحتك وعملك.</div>
-            <div className='text-white text-2xl text-right border-b-2 border-white py-3'>عادل شكل</div>
+            <div className='text-white text-2xl text-right border-b-2 border-white py-3'>محمد أحمد</div>
         </div>
     </div>
   );
